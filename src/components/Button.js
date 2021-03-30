98cb0311d9655a9f821bcb79ef43e822557881bb
Elias Castañeda <e71az@icloud.com>
2021-03-30 06:58:38 -0700

    Added styling to root and enabled button to accept bootstrap color class

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import { PropTypes } from 'prop-types';
 import Button from 'react-bootstrap/Button';
 
-function renderButton({ name = 'button2' }) {
-  return <Button className="btn-danger">{name}</Button>;
+function renderButton({ name = 'button2', color = 'light' }) {
+  return <Button className={`btn btn-${color}`}>{name}</Button>;
 }
 
 renderButton.propTypes = {
