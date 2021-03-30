bed474e57c87ec136d9421c781b0f33cb82e1e7d
Elias Castañeda <e71az@icloud.com>
2021-03-30 05:20:01 -0700

    Added 4 components, button, app, buttonPanel and display. Rendered btnpanel and display in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './style.css';
+// import './style.css';
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+// function App() {
+//   return <h1>Hello World</h1>;
+// }
 
-export default App;
+// export default App;
