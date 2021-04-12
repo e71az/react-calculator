028b6ec7045fb206eca1455c8accc71bf984e632
Elias Castañeda <e71az@icloud.com>
2021-04-12 02:41:45 -0600

    Added content to quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Quote = () => <h1>Quote</h1>;
+const Quote = () => (
+  <div className="home-container ml-5 mt-5">
+    <h1>
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding. –William Paul Thurston
+    </h1>
+  </div>
+);
 
 export default Quote;
